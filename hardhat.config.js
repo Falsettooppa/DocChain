3d99f9e7b1330c4c3f02f502f553319258fdf9d3
Falsettooppa <abiodunolutoye1912@gmail.com>
2026-04-01 00:29:11 +0100

    Update ChainVault hackathon version

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,11 +4,11 @@ require("dotenv").config();
 module.exports = {
   paths: {
     artifacts: "./artifacts",
-},
-  solidity: "0.8.0", 
+  },
+  solidity: "0.8.0",
   networks: {
     scrollSepolia: {
-      url: process.env.SCROLL_RPC_URL,
+      url: process.env.RPC_URL,
       chainId: 534351,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,28 +4,52 @@ const fs = require("fs");
 const path = require("path");
 
 async function main() {
-    const web3 = new Web3(process.env.SCROLL_RPC_URL);
-    const privateKey = process.env.PRIVATE_KEY;
+    const rpcUrl = process.env.RPC_URL;
+    const privateKey = process.env.PRIVATE_KEY.startsWith("0x")
+        ? process.env.PRIVATE_KEY
+        : `0x${process.env.PRIVATE_KEY}`;
+
+    if (!rpcUrl) {
+        throw new Error("RPC_URL is missing in .env");
+    }
+
+    if (!process.env.PRIVATE_KEY) {
+        throw new Error("PRIVATE_KEY is missing in .env");
+    }
+
+    const web3 = new Web3(rpcUrl);
+
     const account = web3.eth.accounts.privateKeyToAccount(privateKey);
     web3.eth.accounts.wallet.add(account);
     web3.eth.defaultAccount = account.address;
 
     console.log("Deploying contract with account:", account.address);
 
-    const contractPath = path.resolve(__dirname, "../artifacts/contracts/DocumentSharing.sol/DocumentSharing.json");
+    const contractPath = path.resolve(
+        __dirname,
+        "../artifacts/contracts/DocumentSharing.sol/DocumentSharing.json"
+    );
+
     const { abi, bytecode } = JSON.parse(fs.readFileSync(contractPath, "utf8"));
 
     const documentSharingContract = new web3.eth.Contract(abi);
 
-    const deployedContract = await documentSharingContract.deploy({ data: bytecode }).send({
-        from: account.address,
-        gas: 3000000,
-        gasPrice: await web3.eth.getGasPrice(),
-    });
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const deployedContract = await documentSharingContract
+        .deploy({ data: bytecode })
+        .send({
+            from: account.address,
+            gas: 3000000,
+            gasPrice,
+        });
 
     console.log("DocumentSharing contract deployed at:", deployedContract.options.address);
 
-    fs.writeFileSync("./contract-address.json", JSON.stringify({ address: deployedContract.options.address }, null, 2));
+    fs.writeFileSync(
+        path.resolve(__dirname, "../contract-address.json"),
+        JSON.stringify({ address: deployedContract.options.address }, null, 2)
+    );
 }
 
 main().catch((error) => {
